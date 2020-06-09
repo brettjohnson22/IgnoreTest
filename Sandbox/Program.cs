387f6d4aa026bcca274b296d6d9cded02227666d
Brett Johnson <br.onson@outlook.com>
2020-06-09 16:33:32 -0500

    init

---
+++ Sandbox/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int i = 0; i < 101; i++)
+            {
+
+                var result = (i % 3 == 0 && i % 5 == 0) ? "fizzbuzz" : (i % 3 == 0) ? "fizz" : (i % 5 == 0) ? "buzz" : i.ToString();
+                Console.WriteLine(result);
+
+            }
+
+
+            
+
+
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
